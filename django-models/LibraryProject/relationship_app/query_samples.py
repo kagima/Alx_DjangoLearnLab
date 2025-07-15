fc1eb9f7993e6ebd88a4ca5603a4236745749bcb
alchemist1111 <emoyoeugine@gmail.com>
2025-07-15 12:58:15 +0300

    added list all books in a library query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,4 +10,19 @@ def get_books_by_author(author_name):
             print(f" - {book.title}")
         return author.books.all()
     except Author.DoesNotExist:
+        print(f"Author '{author_name}' does not exist.")
         return None
+    
+# Query 2
+# List all books in a library.
+def get_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        books = library.books.all()
+        print(f"Books in {library_name}:")
+        for book in books:
+            print(f" - {book.title}")
+        return books    
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' does not exist.")
+        return None         
