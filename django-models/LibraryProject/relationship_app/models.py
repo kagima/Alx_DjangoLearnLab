e526c364d72db33e6f5322223dec752fcf9c8cb3
alchemist1111 <emoyoeugine@gmail.com>
2025-07-15 12:11:32 +0300

    added author model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Author model
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
