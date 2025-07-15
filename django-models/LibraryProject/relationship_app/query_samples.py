fbf76eec4676c700b101a6e7bf45a6ebb1e5e61c
alchemist1111 <emoyoeugine@gmail.com>
2025-07-15 13:05:14 +0300

    added retrieve the librarian for a library query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -4,7 +4,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query to get all books by a specific author
 def get_books_by_author(author_name):
     try:
-        author = Author.objects.get(name=author_name)
+        author = Author.objects.get(name=author)
         print(f"Books by {author_name}:")
         for book in author.books.all():
             print(f" - {book.title}")
@@ -25,4 +25,15 @@ def get_books_in_library(library_name):
         return books    
     except Library.DoesNotExist:
         print(f"Library '{library_name}' does not exist.")
-        return None         
+        return None 
+    
+# Query 3
+# Retrieve the librarian for a library.
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        librarian = library.librarians
+        return librarian
+    except Library.DoesNotExist:
+        print(f"Library '{library_name}' does not exist.")
+        return None 
