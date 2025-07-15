cc75f6f2a6ed4879ed72a6a4ba2e470a9ecf5f24
alchemist1111 <emoyoeugine@gmail.com>
2025-07-15 12:14:59 +0300

    added Book model

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -6,3 +6,11 @@ class Author(models.Model):
     
     def __str__(self):
         return self.name
+    
+# Book model
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    
+    def __str__(self):
+        return self.title 
