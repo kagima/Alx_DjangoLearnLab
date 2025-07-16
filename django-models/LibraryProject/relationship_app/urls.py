c2b35b052434be7c9f503646d29c08e42449df34
alchemist1111 <emoyoeugine@gmail.com>
2025-07-16 11:14:40 +0300

    added urls for admin, member, and librarian only views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,5 +8,8 @@ urls_patterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='application_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='application_app/logout.html'), name='logout'),  
+    path('logout/', LogoutView.as_view(template_name='application_app/logout.html'), name='logout'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),  
 ]
