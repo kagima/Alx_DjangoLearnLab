4c761ee63a6d47d9d63ec4aa949ab5b00680a372
alchemist1111 <emoyoeugine@gmail.com>
2025-07-23 10:53:56 +0300

    added custom user manager and registerd the custom user model in the admin

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -11,9 +11,41 @@ class Book(models.Model):
     def __str__(self):
         return self.title
     
+    
+    
+# Custom User Manager
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        """
+        Create and return a regular user with an email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with an email, password, and extra fields.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
+    
+    
+     
+# Custom user Model
 class CustomUser(AbstractUser):
     # Added fields
     date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True) 
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    def __str__(self):
+        return self.username
+    
+    objects = CustomUserManager() 
     
