bacd7e2e562d9e4e9358788f46863b823d3553fd
alchemist1111 <emoyoeugine@gmail.com>
2025-07-24 23:54:36 +0300

    Updates

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,14 +1,14 @@
 from django import forms
 from .models import Book
 
-class BookForm(forms.ModelForm):
+class ExampleForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'published_date']
-
-    # You can also add custom validation methods
+        
+    # Custom validation example
     def clean_title(self):
         title = self.cleaned_data['title']
         if len(title) < 3:
-            raise forms.ValidationError('Title must be at least 3 characters long.')
+            raise forms.ValidationError("Title must be at least 3 characters long.")
         return title
