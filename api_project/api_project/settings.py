d5e508ffdb775a978c8fe4322d57e2b8af7727a2
alchemist1111 <emoyoeugine@gmail.com>
2025-07-31 23:23:10 +0300

    Added rest auth token in settings installed apps

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST Framework
+    'rest_authtoken',  # Django REST Framework Token Authentication
     'api',  # Your API app
 ]
 
