72e0621197a71a06e6a0630078aa8ad8eaf2ced4
alchemist1111 <emoyoeugine@gmail.com>
2025-08-01 00:06:05 +0300

    Updates

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # Django REST Framework
     'rest_authtoken',  # Django REST Framework Token Authentication
+    'rest_framework.authtoken',  # Token authentication support
     'api',  # Your API app
 ]
 
