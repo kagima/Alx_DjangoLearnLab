6dfb13de31b111c34327416418047217282a117b
alchemist1111 <emoyoeugine@gmail.com>
2025-07-31 18:45:26 +0300

    Implemented book view set urls

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# Router and registration for the BookViewSet
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  # URL for listing books
+    path('', include(router.urls)),  # Include the router URLs for BookViewSet
 ]
