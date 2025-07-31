c821f1241204f9b0b0589d168746741acf0fcfba
alchemist1111 <emoyoeugine@gmail.com>
2025-07-31 18:37:56 +0300

    Implemented class for book view set

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,19 @@
 from rest_framework import generics
 from .models import Book
+from rest_framework import ViewSets
 from .serializers import BookSerializer
 
 # Class-based view to handle book listing
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Fetch all book records
+    serializer_class = BookSerializer  # Use the BookSerializer for serialization
+
+
+# Book set view
+class BookViewSet(ViewSets.ModelViewSet):
+    """"
+      Book set view that handles all the CRUD operations for books.
+      
+    """
+    queryset = Book.objects.all() # Fetch all book records
     serializer_class = BookSerializer  # Use the BookSerializer for serialization
