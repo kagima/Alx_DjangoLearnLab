a8235686598e2a86c7a5afee176c9133c02ff260
alchemist1111 <emoyoeugine@gmail.com>
2025-08-10 01:15:29 +0300

    Updates

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics, filters
 from rest_framework.exceptions import ValidationError
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 # Book List View and permissions
