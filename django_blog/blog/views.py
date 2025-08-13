59a3fb6bd84c6fc7c9e660849804493be7685f54
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 02:28:26 +0300

    Added the register view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import CustomUserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form':form})        
