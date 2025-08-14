f9a5a98b8a8cc871e1114dd4e2a2b04e118994ac
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 06:48:41 +0300

    Added urls for register,login and logout

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -37,13 +37,13 @@ def logout(request):
     return redirect('login') 
 
 # Profile management
-@login_required
-def profile(request):
-    if request.method == 'POST':
-        form = UserChangeForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return redirect('profile')
-    else:
-        form = UserChangeForm(instance=request.user)
-    return render(request, 'blog/profile.html', {'form': form})               
+# @login_required
+# def profile(request):
+#     if request.method == 'POST':
+#         form = UserChangeForm(request.POST, instance=request.user)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('profile')
+#     else:
+#         form = UserChangeForm(instance=request.user)
+#     return render(request, 'blog/profile.html', {'form': form})               
