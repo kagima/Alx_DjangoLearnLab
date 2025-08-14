08371cd07a570d468758b093cd8fefdf4a443667
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 08:05:50 +0300

    Added post form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import UserProfile
+from .models import Post
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -13,4 +14,15 @@ class RegisterForm(UserCreationForm):
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
-        fields = ['bio', 'profile_picture']        
+        fields = ['bio', 'profile_picture']  
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content'] 
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+              
