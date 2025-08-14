e7420392dcdc7022e8b1d171a837d3860127efe2
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 08:58:25 +0300

    Performed migrations

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     # Posts CRUD
     path('posts/', views.PostListView.as_view(template_name='blog/post_list.html'), name='posts'),
     path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(template_name='blog/post_detail.html'), name='post-detail'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
     path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
