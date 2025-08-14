055cbbe2d51bebaeb0cdf2bfc266023b88a933ad
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 09:44:05 +0300

    Modefied post form to include tags

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -21,8 +22,9 @@ class UserProfileForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content'] 
+        fields = ['title', 'content', 'tags']
         widgets = {
+            'tags': TagWidget(attrs={'class': 'form-control'}),
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
