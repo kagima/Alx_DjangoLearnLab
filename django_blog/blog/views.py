7bd0fc54b8eb2efc01a7d3fb68e550cf4d41f1c8
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 06:33:24 +0300

    Added login, logout and profile views and changed forms

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,16 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
-from .forms import CustomUserCreationForm
+from .forms import RegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.decorators import login_required
 
 # Registration view
 def register(request):
     if request.method == 'POST':
-        form = CustomUserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            form.save()
             return redirect('login')
     else:
-        form = CustomUserCreationForm()
-    return render(request, 'blog/register.html', {'form':form})        
+        form = RegisterForm()
+    return render(request, 'blog/register.html', {'form':form})
+
+
+# Login view
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form':form})
+
+
+# Logout view
+def logout(request):
+    logout(request)
+    return redirect('login') 
+
+# Profile management
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserChangeForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})               
