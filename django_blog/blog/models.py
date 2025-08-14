9b79d2f6c88c16b4062b641f356af11c9eb7c373
alchemist1111 <emoyoeugine@gmail.com>
2025-08-14 09:12:46 +0300

    Added comments model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Post model
 class Post(models.Model):
@@ -17,4 +18,19 @@ class UserProfile(models.Model):
     profile_picture = models.ImageField(upload_to='profile_pictures', blank=True) 
     
     def __str__(self):
-        return f"{self.user.username}'s profile"   
+        return f"{self.user.username}'s profile"
+    
+    
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
+
+    def get_absolute_url(self):
+        # Redirect back to the post detail page after comment CRUD
+        return reverse('post-detail', kwargs={'pk': self.post.pk})       
