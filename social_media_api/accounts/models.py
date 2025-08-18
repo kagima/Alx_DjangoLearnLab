b8fe71eb75929abd1a7cb0de998cb9e217b9ec8f
alchemist1111 <emoyoeugine@gmail.com>
2025-08-19 00:43:36 +0300

    Add custom user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager 
 
-# Create your models here.
+# User model extended from AbstractUser
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    followers = models.ManyToManyField('self', blank=True, symmetrical=False)
+    
+    def __str__(self):
+        return self.username
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'accounts',
+    
+    # Third party apps
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +126,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom model configuration
+AUTH_USER_MODEL = 'accounts.user'
+
