64d398d16ecd7be8ca7aa76ebf00905e79b67e15
alchemist1111 <emoyoeugine@gmail.com>
2025-08-20 01:55:59 +0300

    Add post viewset

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsAuthorOrReadOnly
 
-# Create your views here.
+
+# Post ViewSet
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
