7c8ec04e16b065191f0b25cbbad2a619973613d1
alchemist1111 <emoyoeugine@gmail.com>
2025-08-20 01:46:23 +0300

    Add comment serializer

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -12,3 +12,13 @@ class PostSerializer(serializers.ModelSerializer):
          model = Post
          fields = ['id', 'author', 'title', 'content', 'created_at', 'updated_at']
          read_only_fields = ['id', 'author', 'created_at', 'updated_at']
+         
+# Comments serializer
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField(read_only=True)  # Display the username, read-only
+    post = serializers.PrimaryKeyRelatedField(queryset=Post.objects.all())
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'author', 'post', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'author', 'created_at', 'updated_at']
