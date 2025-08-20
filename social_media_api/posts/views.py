34f87a628f49728c6d7c5b9347bf402833ced85d
alchemist1111 <emoyoeugine@gmail.com>
2025-08-20 08:28:23 +0300

    Add feed view

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 from rest_framework import filters
+from rest_framework.response import Response
+from django.db.models import Q
+from rest_framework.views import APIView
 
 
 # Post ViewSet
@@ -25,4 +28,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)        
+        serializer.save(author=self.request.user)  
+        
+# Feed view
+class FeedView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request):
+        followed_users = request.user.following.all()
+        posts = Post.objects.filter(Q(author__in=followed_users) | Q(author=request.user)).order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)              
