5f4584de79b65308a9cbcb2f51b921ef1e576c37
alchemist1111 <emoyoeugine@gmail.com>
2025-08-20 08:37:18 +0300

    Fixed follow field in user model

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', blank=True, related_name='followed_by', symmetrical=False)
+    following = models.ManyToManyField('self', blank=True, related_name='followed_by', symmetrical=False)
     
     def __str__(self):
         return self.username
