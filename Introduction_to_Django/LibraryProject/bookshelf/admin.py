d9fd13e076c2e4b62866ac8bd1c6d5a086012d9c
chrispus <kagimachris@gmail.com>
2025-09-06 19:05:48 +0300

    Task 2: Register Book model in admin with list display, filter, and search

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Register the Book model with the admin
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns to show
+    list_filter = ('author', 'publication_year')            # Filters on right
+    search_fields = ('title', 'author')                     # Search bar
+
